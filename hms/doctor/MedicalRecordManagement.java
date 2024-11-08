1272ad497e162d207395b302b80af2e8a7795e97
dayuhnah <dayanamarsya@gmail.com>
2024-11-08 16:37:59 +0800

    update medicalrecordmanagement

--- hms/doctor/MedicalRecordManagement.java
+++ hms/doctor/MedicalRecordManagement.java
@@ -29,43 +29,6 @@ public class MedicalRecordManagement{
         this.medications = new ArrayList<>();
     }
 
-    public String getPatientId(){
-        return patientId;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public String getBloodType(){
-        return bloodType;
-    }
-    
-    public List<String> getDiagnoses(){
-        return diagnoses;
-    }
-
-    public List<String> getTreatments(){
-        return treatments;
-    }
-
-    public List<String> getMedications(){
-        return medications;
-    }
-
-    public void addDiagnosis(String diagnosis){
-        diagnoses.add(diagnosis);
-    }
-
-    public void addTreatmentt(String treatment){
-        treatments.add(treatment);
-    }
-
-    public void addMedication(String medication){
-        medications.add(medication);
-    }
-
-
     public void viewMedicalRecord(String patientId, List<Map<String, String>> patientList){
         // MedicalRecordManagement record = medicalRecords.get(patientId);
 
