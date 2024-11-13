7e431de409a5b033b6231402ba02512fb7786349
ymkatie <160112803+ymkatie@users.noreply.github.com>
2024-11-14 01:22:11 +0800

    Update Pharmacist.java

--- hms/pharmacist/Pharmacist.java
+++ hms/pharmacist/Pharmacist.java
@@ -3,42 +3,74 @@ package pharmacist;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Pharmacist extends User {
+public class Pharmacist {
+    private String pharmacistId;
     private String name;
-    private String contactInfo;
-    private List<Prescription> prescriptionList;
+    private String gender;
+    private String age;
+    private String contactNumber;
+    private List<Prescription> managedPrescriptions;
 
-    public Pharmacist(String userId, String name) {
-        super(userId, "Pharmacist");
+    public Pharmacist(String pharmacistId, String name, String age, String gender, String contactNumber) {
+        this.pharmacistId = pharmacistId;
         this.name = name;
-        this.prescriptionList = new ArrayList<>();
+        this.age = age;
+        this.gender = gender;
+        this.contactNumber = contactNumber;
+        this.managedPrescriptions = new ArrayList<>();
+    }
+
+    public String getPharmacistId() {
+        return pharmacistId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public List<Prescription> getManagedPrescriptions() {
+        return managedPrescriptions;
+    }
+
+    public void setManagedPrescriptions(List<Prescription> managedPrescriptions) {
+        this.managedPrescriptions = managedPrescriptions;
     }
 
     public void addPrescription(Prescription prescription) {
-        prescriptionList.add(prescription);
+        this.managedPrescriptions.add(prescription);
     }
 
     public void viewPrescriptions() {
-        System.out.println("Viewing all prescriptions for pharmacist: " + name);
-        for (Prescription prescription : prescriptionList) {
+        System.out.println("Prescriptions managed by Pharmacist " + getName() + ":");
+        for (Prescription prescription : managedPrescriptions) {
             System.out.println(prescription);
         }
     }
 
-    public void updatePrescriptionStatus(String prescriptionId, String status) {
-        for (Prescription prescription : prescriptionList) {
+    public void updatePrescriptionStatus(String prescriptionId, String newStatus) {
+        for (Prescription prescription : managedPrescriptions) {
             if (prescription.getPrescriptionId().equals(prescriptionId)) {
-                prescription.updateStatus(status);
-                System.out.println("Prescription " + prescriptionId + " status updated to: " + status);
-                return;
+                prescription.updateStatus(newStatus);
+                System.out.println("Updated Prescription " + prescriptionId + " to " + newStatus);
             }
         }
-        System.out.println("Prescription ID not found: " + prescriptionId);
     }
 
     @Override
-    public void displayUserInfo() {
-        super.displayUserInfo();
-        System.out.println("Name: " + name);
+    public String toString() {
+        return "Pharmacist ID: " + getPharmacistId() + ", Name: " + getName() + ", Age: " + getAge() + ", Gender: " + getGender()
+                + ", Contact Number: " + getContactNumber();
     }
 }
